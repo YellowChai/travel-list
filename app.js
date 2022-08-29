a8868c460db19235fdd71a29aa5dfba63a965812
alice <aliceahuh213@gmail.com>
2022-08-29 16:51:40 -0400

    add lines to display default img when api returns empty

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const travelButton = document.querySelector(".list-button");
 const travelList = document.querySelector(".travel-list");
 const defaultImage ="https://image.shutterstock.com/shutterstock/photos/1094945555/display_1500/stock-photo-blue-suitcase-with-sun-glasses-hat-and-camera-on-pastel-blue-background-travel-concept-minimal-1094945555.jpg";
 
-
-
 // Event Listeners
 form.addEventListener("submit", addPlace);
 
@@ -27,57 +25,48 @@ function addPlace(event) {
     const newDest = document.createElement("li");
     newDest.innerText = destInput.value;
     newDest.classList.add('new-dest')
-    placeDiv.appendChild(newDest);
-    console.log(newDest)
 
     //Create description List
     const newDescription = document.createElement('li');
     newDescription.innerText = descriptionInput.value;
     newDescription.classList.add('new-description')
-    placeDiv.appendChild(newDescription);
-    console.log(newDescription)
 
     //Create location List
     const newPlace = document.createElement('li');
     newPlace.innerText = locationInput.value;
     newPlace.classList.add('new-place')
-    placeDiv.appendChild(newPlace);
-    console.log(newPlace)
 
-    
-
+    //Create img by fetching 
     const requestUrl = 'https://api.unsplash.com/search/photos?query=' + newPlace.innerHTML + '-' + newDest.innerHTML+ '&client_id=Q41Uq_nT8K_r-7vPg_E035mTUIQwJbMvx18L9scLpfs';
-    // Create Img
     const newImage = document.createElement('img');
     description.addEventListener('click', fetchImg());
-    // newImage.src = (imageInput.value === "") ? defaultImage : imageInput.value; 
     async function fetchImg(){
         await fetch(requestUrl)
         .then(response => {
-            return response.json();
+            return response.json();            
         })
         .then(data => {
-            let img = data.results[0].urls.regular;
-            newImage.src = img;
-            newImage.classList.add('new-image')
-            
+            let img = data.results[0]? data.results[0].urls.regular : defaultImage;
+            newImage.src = img 
+            newImage.classList.add('new-image')            
         })
+        
     }
-
-    
+ 
     //Edit Button
     const editButton = document.createElement('button');
     editButton.innerText = "Edit";
     editButton.classList.add("edit-btn");
-    
-    
+      
     //Delete Button
     const deleteButton = document.createElement('button');
     deleteButton.innerText = "Delete";
-    deleteButton.classList.add("delete-btn");
-    
+    deleteButton.classList.add("delete-btn"); 
 
     //Append to list
+    placeDiv.appendChild(newDest);
+    placeDiv.appendChild(newPlace);
+    placeDiv.appendChild(newDescription);
     placeDiv.appendChild(newImage);
     placeDiv.appendChild(editButton);
     placeDiv.appendChild(deleteButton);
@@ -85,17 +74,14 @@ function addPlace(event) {
     travelList.appendChild(placeDiv);
 
     //Clear travel input value
-   
     locationInput.value = "";
     destInput.value = "";
     descriptionInput.value = "";    
 }
 
-
 function deleteItem(e){
     const item = e.target;
     // Delete list
-
     if(item.classList[0] === 'delete-btn'){
         const place = item.parentElement;
         place.remove();
